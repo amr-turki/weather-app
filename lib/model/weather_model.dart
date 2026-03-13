45bf03cbcf06cfe63afd5222fc1350bc8d52917a
Amr Turki <amrturki79@gmail.com>
2026-03-13 10:13:09 +0200

    Finish Weather Model

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,33 @@
+class WeatherModel {
+  final String City;
+  final String Date;
+  final String image;
+
+  final String Temp;
+  final double minTemp;
+  final double maxTemp;
+  final String statusWeather;
+
+  const WeatherModel({
+    required this.City,
+    required this.Date,
+    required this.Temp,
+    required this.image,
+    required this.maxTemp,
+    required this.minTemp,
+    required this.statusWeather,
+  });
+
+  factory WeatherModel.fromjson(json) {
+    return WeatherModel(
+      City: json['location']['name'],
+      Date: json['current']['last_updated'],
+      Temp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+      image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
+      maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+      statusWeather:
+          json['forecast']['forecastday'][0]['day']['condition']['text'],
+    );
+  }
+}
