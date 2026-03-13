4d0feeb0b13599f13d41fd41584ac6930889dcb1
Amr Turki <amrturki79@gmail.com>
2026-03-13 10:42:46 +0200

    Provide Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_application/cubit/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather_application/view/home_screen.dart';
 
 void main() {
@@ -10,6 +12,11 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+    return BlocProvider(
+      create: (context) {
+        return GetWeatherCubit();
+      },
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()),
+    );
   }
 }
