0cd40523ef606a631adb41ee827628009593247d
Amr Turki <amrturki79@gmail.com>
2026-03-13 10:22:49 +0200

    Finish Weather Service

---
+++ lib/service/weather_service.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_application/model/weather_model.dart';
+
+class WeatherService {
+  final Dio dio;
+  WeatherService(this.dio);
+
+  Future<WeatherModel?> FetchWeatherDate({required String city}) async {
+    final dio = Dio();
+
+    final String baseurl = 'http://api.weatherapi.com/v1';
+    final String apikey = '691b705741dd4e5392f105346261202';
+
+    try {
+      final response = await dio.get(
+        '$baseurl/forecast.json?key$apikey=&q$city&days=2&aqi=no&alerts=no',
+      );
+      WeatherModel model = WeatherModel.fromjson(response.data);
+
+      return model;
+    } on DioException catch (e) {
+      final String message =
+          e.response?.data['error']['message'] ??
+          'Oops! There seems to be a problem. Please try again.';
+      throw Exception(message);
+    } on Exception catch (e) {
+      throw Exception('Oops! There seems to be a problem. Please try again.');
+    }
+  }
+}
